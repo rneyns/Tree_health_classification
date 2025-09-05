ccb88ea73ac36e8e54acde32de1bab9a73e32eb3
rneyns <56020455+rneyns@users.noreply.github.com>
2025-09-05 13:05:49 +0200

    added hardware selector

--- Train_model.py
+++ Train_model.py
@@ -42,9 +42,19 @@ if __name__ == "__main__":
     args = get_arguments()
 
     #setup_seed(args.random_seed)
-    device = torch.device('cuda:' + str(args.gpu) if args.use_cuda else 'cpu')
-    device = torch.device("mps")
-    print("Using device: ", device)
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")  # default: cuda:0
+        print(f"Using CUDA device: {torch.cuda.get_device_name(0)}")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")  # Apple Silicon (Metal Performance Shaders)
+        print("Using MPS device")
+    else:
+        device = torch.device("cpu")
+        print("Using CPU")
+
+    # usage
+    print("Selected device:", device)
 
     # 1) Load the data
     trainloader, validloader, testloader, cat_dims, con_idxs, y_dim, DOY, w0_norm, w1_norm, args = dataloader_init(args)
