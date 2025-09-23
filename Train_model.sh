f0005aed469ee16670b33c313bb2bbe61769e037
rneyns <56020455+rneyns@users.noreply.github.com>
2025-09-23 13:12:15 +0200

    Update Train_model.sh

--- Train_model.sh
+++ Train_model.sh
@@ -8,7 +8,7 @@
 
 #SBATCH --time=1:00:00
 
-#SBATCH --partition=pascal_gpu
+#SBATCH --partition=ampere_gpu
 
 
 
@@ -16,8 +16,11 @@
 
 ml purge
 
-cd /user/brussel/104/vsc10421/Tree_health_classification
-source tree_health/bin/activate
+cd /data/brussel/104/vsc10421/
+python3 -m venv tree_health_venv --system-site-packages
+source tree_health_venv/bin/activate
+
+python3 -m pip install imbalanced-learn
 
 ml load scikit-learn/1.5.2-gfbf-2024a
 ml load Pillow/10.4.0-GCCcore-13.3.0
@@ -28,6 +31,7 @@ ml load wandb/0.20.1-GCC-13.3.0
 ml load einops/0.8.1-GCCcore-13.3.0
 ml load PyTorch/2.6.0-foss-2024a-CUDA-12.6.0
 
+cd /data/brussel/104/vsc10421/Tree_health_classification
 
 
-tree_health/bin/python -u Train_model.py  -i "/data/brussel/104/vsc10421/tree health classification/output_patches/output_patches_hydra/" -multi "/data/brussel/104/vsc10421/tree health classification/output_patches/5_species_other/" -lh species_code -idh tree_id -w 120 -ts 17 -nc 6 --epochs 10 --alpha 0.5 --task 'multiclass' --undersample --spatio_temp --batch_size 16
+python -u Train_model.py  -i "/data/brussel/104/vsc10421/tree health classification/output_patches/output_patches_hydra/" -multi "/data/brussel/104/vsc10421/tree health classification/output_patches/5_species_other/" -lh species_code -idh tree_id -w 120 -ts 17 -nc 6 --epochs 10 --alpha 0.5 --task 'multiclass' --undersample --spatio_temp --batch_size 16
