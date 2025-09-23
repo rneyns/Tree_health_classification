17e13754f1b96ebacce6c138d2b7c876fdd5d899
rneyns <56020455+rneyns@users.noreply.github.com>
2025-09-23 15:36:42 +0200

    Update Train_model.py

--- Train_model.py
+++ Train_model.py
@@ -37,6 +37,8 @@ if __name__ == "__main__":
     import wandb
 
     print(f"The benchmark setting is on: {torch.backends.cudnn.benchmark}")
+    torch.backends.cudnn.benchmark = True
+    print(f"The benchmark setting is on: {torch.backends.cudnn.benchmark}")
     args = get_arguments()
 
     #setup_seed(args.random_seed)
