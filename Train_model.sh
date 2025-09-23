ffd436968099e81f338939b04ac056d61b13a612
rneyns <56020455+rneyns@users.noreply.github.com>
2025-09-23 13:37:10 +0200

    Update Train_model.sh

--- Train_model.sh
+++ Train_model.sh
@@ -31,7 +31,7 @@ ml load wandb/0.20.1-GCC-13.3.0
 ml load einops/0.8.1-GCCcore-13.3.0
 ml load PyTorch/2.6.0-foss-2024a-CUDA-12.6.0
 
-cd /data/brussel/104/vsc10421/Tree_health_classification
+cd /user/brussel/104/vsc10421/Tree_health_classification/
 
 
 python -u Train_model.py  -i "/data/brussel/104/vsc10421/tree health classification/output_patches/output_patches_hydra/" -multi "/data/brussel/104/vsc10421/tree health classification/output_patches/5_species_other/" -lh species_code -idh tree_id -w 120 -ts 17 -nc 6 --epochs 10 --alpha 0.5 --task 'multiclass' --undersample --spatio_temp --batch_size 16
